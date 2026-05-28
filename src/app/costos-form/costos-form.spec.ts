86e214d9a53b50be4a9fdb36bec0f86eaa52a143
carlos310 <charlirodriguezvanegas@gmail.com>
2026-05-27 21:12:46 -0500

    creacion de forms para costos, ingresos,gastos y ventas

---
+++ src/app/costos-form/costos-form.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Costos } from './costos-form';
+
+describe('Costos', () => {
+  let component: Costos;
+  let fixture: ComponentFixture<Costos>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Costos],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Costos);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
