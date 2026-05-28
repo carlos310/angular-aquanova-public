50202221e9fe08e5d842df4917db4aed7db00c2a
carlos310 <charlirodriguezvanegas@gmail.com>
2026-05-27 22:13:45 -0500

    Delete src/app/costos-form directory
    delete duplicate

--- src/app/costos-form/costos-form.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-costos-form',
-  imports: [],
-  templateUrl: './costos-form.html',
-  styleUrl: './costos-form.css',
-})
-export class Costos {}
