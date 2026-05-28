c5da96d6506b24a1411085584cf39497cc9faacc
carlos310 <charlirodriguezvanegas@gmail.com>
2026-05-27 22:09:52 -0500

    cambio de ubicacion de los forms para ponerlos dentro de los componentes

---
+++ src/app/components/gastos-form/gastos-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gastos-form',
+  imports: [],
+  templateUrl: './gastos-form.html',
+  styleUrl: './gastos-form.css',
+})
+export class Gastos {}
---
+++ src/app/components/ingresos-main-form/ingresos-main-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingresos-main-form',
+  imports: [],
+  templateUrl: './ingresos-main-form.html',
+  styleUrl: './ingresos-main-form.css',
+})
+export class IngresosMainForm {}
